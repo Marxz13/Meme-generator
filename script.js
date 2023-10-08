d2eada48d98dbd4e518dec763d35c13944b145ad
Marz <141631535+Marxz13@users.noreply.github.com>
2023-10-08 15:04:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const generateMemeBtn = document.querySelector(
+    ".meme-generator .generate-meme-btn"
+  );
+  const memeImage = document.querySelector(".meme-generator img");
+  const memeTitle = document.querySelector(".meme-generator .meme-title");
+  const memeAuthor = document.querySelector(".meme-generator .meme-author");
+  
+  const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by: ${author}`;
+  };
+  
+  const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes")
+      .then((response) => {
+        if(response.ok){
+            return response.json();
+        } else{
+            throw new Error('HTTP error! status: ${response.status}')
+        }
+    }).then((data) => {
+        updateDetails(data.url, data.title, data.author);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+}
+  
+  generateMemeBtn.addEventListener("click", generateMeme);
+  
+  generateMeme();
